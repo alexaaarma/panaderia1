0287ec365695aedb02dcdd747c407b26092a988d
C3 E3 <C3 E3@DESKTOP-833BVMC>
2024-06-05 10:45:32 -0600

    pannn

--- EjercicioPanClie/src/main/java/com/mx/cbtis/controller/PanaderiaControlador.java
+++ EjercicioPanClie/src/main/java/com/mx/cbtis/controller/PanaderiaControlador.java
@@ -16,11 +16,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 
 import com.mx.cbtis.modelo.Pedidos;
+import com.mx.cbtis.modelo.Producto;
 
 
 @Controller
@@ -65,6 +67,54 @@ public class PanaderiaControlador {
 		HttpEntity<Pedidos> request = new HttpEntity<Pedidos>(pedidos);
 		ResponseEntity<Integer> response = template.exchange(urlservicebd,HttpMethod.DELETE,request,Integer.class);
 		return new ModelAndView("redirect:/pedidos");
+	}
+ @GetMapping("/agregarP")
+	public String getAgregarProducto() {
+		
+		return "agregarP";
+	}
+	
+	@GetMapping("/producto")
+	public String getProducto(Model modelo) {
+		RestTemplate template = new RestTemplate();
+		String urlservicebd = "http://localhost:8081/buscarProductos";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		Producto[] response = template.postForObject(urlservicebd,headers,Producto[].class);
+		List<Producto> producto = Arrays.asList(response);
+		modelo.addAttribute("listaProducto", producto);
+		return "producto";
+	}
+	@PostMapping("/saveProducto")
+	public ModelAndView setSaveProducto(@ModelAttribute Producto producto) {
+		RestTemplate template = new RestTemplate();
+		String urlservicebd = "http://localhost:8081/addProducto";
+		ResponseEntity <Integer> response = template.postForEntity(urlservicebd, producto, Integer.class);
+		
+		return new ModelAndView("redirect:/producto");
+	}
+	
+	@PostMapping("/eliminarProducto")
+	public ModelAndView eliminarProducto(@RequestBody Producto producto) {
+		System.out.println("El ID del Producto es: " + producto.getId());
+		RestTemplate template = new RestTemplate();
+		String urlservicebd = "http://localhost:8081/deleteProducto";
+		HttpEntity<Producto> request = new HttpEntity<Producto>(producto);
+		ResponseEntity <Integer> response = template.exchange(urlservicebd, HttpMethod.DELETE,request, Integer.class);
+
+		return new ModelAndView("redirect:/producto");
+		
+	}
+	@GetMapping("/actualizarProducto")
+	public String updateProducto(@RequestParam(value = "id") int id, Model model) {
+		RestTemplate template = new RestTemplate();
+		Map<String, Integer> params = new HashMap<String, Integer>();
+		params.put("id", id);
+		String urlservicebd = "http://localhost:8081/buscarProducto?id={id}";
+		Producto producto = template.getForObject(urlservicebd, Producto.class, params);
+		model.addAttribute("producto", producto);
+		
+		return "actualizarP";
 	}
  
 }
