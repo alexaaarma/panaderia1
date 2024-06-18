f1894c84088ad15d26f4710c44214130f2fedb38
alexaaarma <alexa22531@cbtis75.edu.mx>
2024-06-18 08:25:23 -0600

    Revert "Delete PanaderiaPedidosApplication.java"
    This reverts commit 152514afa113a7ae4376ffe8b3cd29da64d54a18.

---
+++ EjercicioPanClie/src/main/java/com/mx/cbtis/PanaderiaPedidosApplication.java
@@ -0,0 +1,13 @@
+ package com.mx.cbtis.panaderiapedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanaderiaPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanaderiaPedidosApplication.class, args);
+	}
+
+}
